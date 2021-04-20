c68259cc29296928e9c9cc850602807a73c16551
PiotrPaprzycki <Avalok2015>
2021-04-20 14:49:15 +0200

    Adding: Try with resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,19 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_db?serverTimezone=UTC", "root", "pepe-23");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM pracownik ORDER BY nazwisko");
 
-        while (resultSet.next()) {
-            String imie = resultSet.getString(4);
-            String nazwisko = resultSet.getString(5);
-            System.out.println("Imię: " + imie + " " + "Nazwisko: " + nazwisko);
+        try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_db?serverTimezone=UTC", "root", "pepe-23");
+                Statement statement = connection.createStatement();
+        ) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM pracownik ORDER BY nazwisko");
+
+            while (resultSet.next()) {
+                String imie = resultSet.getString("imie");
+                String nazwisko = resultSet.getString("nazwisko");
+                System.out.println("Imię: " + imie + " " + "Nazwisko: " + nazwisko);
+            }
+
         }
-
     }
 }
