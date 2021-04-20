6fb64f2c6e72a02939c843dd5223429a48a8025c
PiotrPaprzycki <Avalok2015>
2021-04-20 08:46:49 +0200

    First select

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Connection connection = DriverManager.getConnection()
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_db?serverTimezone=UTC", "root", "pepe-23");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM pracownik ORDER BY nazwisko");
+
+        while (resultSet.next()) {
+            String imie = resultSet.getString(4);
+            String nazwisko = resultSet.getString(5);
+            System.out.println("Imię: " + imie + " " + "Nazwisko: " + nazwisko);
+        }
+
     }
 }
